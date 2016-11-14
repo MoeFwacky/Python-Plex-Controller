a2d0882c1d71e8a20fc2cba3a26d8b1a19a427b0
amazingr4b <amazingr4b@gmail.com>
2016-11-13 22:35:46 -0600

    basic schedule changes

--- add_to_cron.py
+++ add_to_cron.py
@@ -3,7 +3,8 @@ import sys
 user = str(sys.argv[1])
 
 workd = "/etc/crontab"
-writeme = "* * * * * * " + user + " python /home/ " + user + "/hasystem/tbn_schedule.py\n@reboot " + user + " python /home/" + user + "/hasystem/piplaystate.py > /dev/null 2>&1 &"
+writeme = "@reboot " + user + " python /home/" + user + "/hasystem/piplaystate.py > /dev/null 2>&1 &"
+writeme2 = "* * * * * * " + user + " python /home/ " + user + "/hasystem/tbn_schedule.py"
 
 with open(workd, "r") as file:
 	checkme1 = file.read()
@@ -18,5 +19,13 @@ else:
 		file.close()
 		print ("A Cron entry has been added for piplaystate.py\n")
 	except Exception:
-		print ("\nError adding piplaystate entry to cron. You need to manually add the following to your cron tab: @reboot " + user + "python /home/" + user + "/hasystem/piplaystate.py >/dev/null 2>&1")
+		print ("Failed to add cron entry for piplaystate.py.")
+if writeme2 not in checkme1:
+		try:
+		with open(workd, "a") as file:
+			file.write(writeme2)
+		file.close()
+		print ("A Cron entry has been added for tbn_schedule.py\n")
+	except Exception:
+		print ("Failed to add cron entry for tbn_schedule.py.")
 
