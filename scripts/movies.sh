911acab52cf60554e5933dbb5357f2ac6b3429f9
Moe Fwacky <moe.fwacky@gmail.com>
2017-05-07 17:45:32 -0700

    adding comment about running daily

--- scripts/movies.sh
+++ scripts/movies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
- 
+# add 0 1 * * * pi sh /location/of/file/movies.sh to /etc/crontab to run this check each day
+
 if [ ${mDate} -eq YYYYMMDD ]; then #replace YYYYMMDD with the desired date in the same format
  python /home/pi/hasystem/system.py addschedule "TITLE" HH:MM today #replace TITLE with the desired title and HH:MM with the time in 24hr format
 fi
